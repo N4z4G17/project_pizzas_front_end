b72d868471fb8b3871e20d8d056e04a10642f580
NAZARENO COSTANTINO <thiagodilauro555@gmail.com>
2025-08-08 18:38:05 -0300

    Work in Branch

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Headland+One&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Oswald:wght@200..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+
+</head>
+<body>
+    
+
+<?php
+//Acá va el código de PHP
+define("USER_REAL", "maria"); //PARA DEFINIR UNA CONSTANTE. EL NOMBRE DEBE ESTAR SIEMPRE EN MAYUSCULA.
+define("PASS_REAL", "maria123456");
+//username
+$usr = $_POST['username'];
+//echo "<br>";
+$pass = $_POST['password'];
+if ($usr == USER_REAL && $pass == PASS_REAL) {
+    echo "<div class='flex flex-col justify-center items-center h-screen'>";
+    echo "<h1 class='font-[Raleway]  font-extrabold text-3xl'> ¡¡¡BIENVENID@ ".USER_REAL."!!! </h1>";
+    echo "<h1 class='font-[Raleway]'>". date('d-m-Y') . "</h1>";
+    "</div>"; //un osito de peluche de Tailwind
+} else {
+    echo "<div class='flex flex-col justify-center items-center h-screen'>";
+    echo "<h1 class='font-[Raleway]  font-extrabold text-3xl'> LOS DATOS INGRESADOS SON INCORRECTOS </h1>";
+    "</div>";
+}
+?>
+
+</body>
+</html>
+
